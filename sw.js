d759681b1b583efba9d29924b238b45e63e11dfd
xbb1993 <xbb_lzu@qq.com>
2026-09-09 10:03:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shift-scheduler-v8';
+const CACHE_NAME = 'shift-scheduler-v9';
 
 const APP_SHELL = [
   './',
@@ -14,7 +14,9 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches
       .open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
+      .then(cache =>
+        cache.addAll(APP_SHELL)
+      )
   );
 
   self.skipWaiting();
@@ -23,17 +25,22 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
 
   event.waitUntil(
-
     caches
       .keys()
       .then(keys =>
         Promise.all(
           keys
-            .filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+            .filter(
+              key => key !== CACHE_NAME
+            )
+            .map(
+              key => caches.delete(key)
+            )
         )
       )
-      .then(() => self.clients.claim())
+      .then(() =>
+        self.clients.claim()
+      )
   );
 });
 
@@ -47,11 +54,11 @@ self.addEventListener('fetch', event => {
     new URL(event.request.url);
 
   /*
-   * HTML / JS / Service Worker
-   * 使用网络优先策略。
+   * HTML / JS / SW：
+   * 网络优先。
    *
    * 这样 GitHub 更新后，
-   * 手机不会长期卡在旧版本。
+   * 手机联网时会优先拿最新代码。
    */
   if(
     url.origin === location.origin &&
@@ -88,7 +95,9 @@ self.addEventListener('fetch', event => {
             .match(event.request)
             .then(response =>
               response ||
-              caches.match('./index.html')
+              caches.match(
+                './index.html'
+              )
             )
         )
     );
@@ -132,7 +141,9 @@ self.addEventListener('fetch', event => {
       })
 
       .catch(() =>
-        caches.match('./index.html')
+        caches.match(
+          './index.html'
+        )
       )
   );
 });
